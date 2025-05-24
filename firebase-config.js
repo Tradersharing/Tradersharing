e0394421e3b92076cf8211c22e1a44874d16e618
Tradersharing <ardiyulistyanto3@gmail.com>
2025-05-24 15:17:25 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Ganti dengan config Firebase proyekmu
+const firebaseConfig = {
+  apiKey: "API_KEY_MU",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://PROJECT_ID-default-rtdb.firebaseio.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+};
+
+// Inisialisasi Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
