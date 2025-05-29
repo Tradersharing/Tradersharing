5f485c8f206a5978cc3d375995eba191417f843f
Tradersharing <ardiyulistyanto3@gmail.com>
2025-05-30 02:17:00 +0700

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("info-anomali").innerText = "EUR/USD\nAnomali: BUY\nTujuan: SELL";
-  document.getElementById("signal-list").innerText = "EURJPY : SELL\nAUDNZD : BUY\nUSDJPY : BUY";
-  document.getElementById("news-list").innerText = "Tidak ada berita penting hari ini.";
-});
